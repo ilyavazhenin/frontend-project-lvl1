5f214c9e482c9efdb36264b64cfd87a776801ce7
Ilya Vazhenin <heyheyftw@gmail.com>
2022-10-16 20:01:21 +0600

    fix: remove comment lines from code

--- src/cli.js
+++ src/cli.js
@@ -4,8 +4,5 @@ const greetings = () => {
     const playerName = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${playerName}!`);
 };
-// const playerName = readlineSync.question('May I have your name? ');
-// console.log(`Hello, ${playerName}!`);
-
 
 export { greetings };
