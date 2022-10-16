af4836add4402917b1d2b2f8fa2b6930838c6872
Ilya Vazhenin <heyheyftw@gmail.com>
2022-10-16 19:54:09 +0600

    feat: add asking user for his name

---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const greetings = () => {
+    const playerName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${playerName}!`);
+};
+// const playerName = readlineSync.question('May I have your name? ');
+// console.log(`Hello, ${playerName}!`);
+
+
+export { greetings };
