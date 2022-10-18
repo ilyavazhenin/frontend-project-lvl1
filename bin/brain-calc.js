a361bb6e7296af0a5b42459d7995e93cc6fa4d28
Ilya Vazhenin <heyheyftw@gmail.com>
2022-10-18 15:42:30 +0600

    feat: add brain-calc game

---
+++ bin/brain-calc.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { startGame } from '../src/brain-calc-logic.js';
+
+startGame();
---
+++ src/brain-calc-logic.js
@@ -0,0 +1,83 @@
+import readlineSync from 'readline-sync';
+import _ from 'lodash';
+
+let playerName;
+let successfullTries = 0;
+let randomThings;
+let playerAnswer;
+let correctAnswer;
+let isWon = true;
+const gameRuleMessage = 'What is the result of the expression?';
+
+const greetUser = () => {
+  console.log('Welcome to the Brain Games!');
+  playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
+};
+
+const showRulesMessage = (message) => {
+  console.log(`${message}`);
+};
+
+const generateRandomThings = () => {
+  const randomNumber1 = Math.round(Math.random() * 100);
+  const randomNumber2 = Math.round(Math.random() * 100);
+
+  const signs = ['+', '-', '*'];
+  let randomSign;
+  const randomStart = 0;
+  const randomEnd = signs.length - 1; // amount of operation in array to generate a random one from
+  const getRandomSign = () => {
+    const randomIndex = _.random(randomStart, randomEnd);
+    randomSign = signs[randomIndex];
+    return randomSign;
+  };
+
+  getRandomSign();
+  randomThings = [randomNumber1, randomNumber2, randomSign];
+  return randomThings;
+};
+
+const whatIsCorrectAnswer = (array) => {
+  switch (array[2]) {
+    case '*': correctAnswer = array[0] * array[1];
+      break;
+
+    case '-': correctAnswer = array[0] - array[1];
+      break;
+
+    case '+': correctAnswer = array[0] + array[1];
+      break;
+
+    default: correctAnswer = null;
+  }
+  return correctAnswer;
+};
+
+const askPlayer = () => {
+  console.log(`Question: ${randomThings[0]} ${randomThings[2]} ${randomThings[1]}`);
+  playerAnswer = readlineSync.question('Your answer: ');
+  const tryAgainMessage = `'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${playerName}!`;
+  if (playerAnswer === String(correctAnswer)) {
+    successfullTries += 1;
+    return console.log('Correct!');
+  }
+
+  successfullTries = 3;
+  isWon = false;
+  return console.log(tryAgainMessage);
+};
+
+export const startGame = () => {
+  greetUser();
+  showRulesMessage(gameRuleMessage);
+  while (successfullTries < 3) {
+    generateRandomThings();
+    whatIsCorrectAnswer(randomThings);
+    askPlayer();
+  }
+
+  if (isWon === true) console.log(`Congratulations, ${playerName}!`);
+};
+
+export default startGame;
