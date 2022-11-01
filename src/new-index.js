69a7bc0eb69a2fabf823857df8aa317c63710812
Ilya Vazhenin <heyheyftw@gmail.com>
2022-11-02 01:28:09 +0600

    fix: start refactoring architecture and brain-even game logic

---
+++ src/new-index.js
@@ -0,0 +1,28 @@
+import readlineSync from 'readline-sync';
+import greetUser from './cli.js';
+
+const ROUNDSCOUNT = 3;
+
+const playGame = (description, gameLogicFunction) => {
+  const playerName = greetUser();
+  console.log(`${description}`);
+
+  let tempRounds = ROUNDSCOUNT;
+  while (tempRounds > 0) {
+    const [generatedQuestion, correctAnswerResult] = gameLogicFunction();
+    console.log(`Question: ${generatedQuestion}`);
+    const playerAnswer = readlineSync.question('Your answer: ');
+
+    if (playerAnswer === String(correctAnswerResult)) {
+      console.log('Correct!');
+      tempRounds -= 1;
+    } else {
+      console.log(`'${playerAnswer}' is wrong answer ;(. Correct answer was '${correctAnswerResult}'.`);
+      console.log(`Let's try again, ${playerName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${playerName}!`);
+};
+
+export default playGame;
