ebdf6a0d96b90bea4bc801e1992e76e208440e60
Ilya Vazhenin <heyheyftw@gmail.com>
2022-11-08 18:56:47 +0400

    fix: remove hardcode for primal numbers, add func that calcs it

--- src/games/brain-prime-logic.js
+++ src/games/brain-prime-logic.js
@@ -3,17 +3,15 @@ import playGame from '../index.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const primeNumbers = [
-  2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-  31, 37, 41, 43, 47, 53, 59, 61, 67,
-  71, 73, 79, 83, 89, 97, 101, 103, 107,
-  109, 113, 127, 131, 137, 139, 149, 151, 157, 163,
-  167, 173, 179, 181, 191, 193, 197, 199,
-];
-
 const isNumberPrime = (num) => {
-  if (primeNumbers.includes(num)) return true;
-  return false;
+  if (num <= 1) return false;
+  if (num % 2 === 0 && num > 2) return false;
+
+  const square = Math.sqrt(num);
+  for (let i = 3; i <= square; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
 };
 
 const getQuestionAndAnswer = () => {
