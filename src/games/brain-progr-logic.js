f8d31786471c3e559e8caf041349aa9d1289844c
Ilya Vazhenin <heyheyftw@gmail.com>
2022-11-14 23:59:53 +0400

    fix: refactor progression function

--- src/games/brain-progr-logic.js
+++ src/games/brain-progr-logic.js
@@ -1,29 +1,27 @@
-import getRandomNumber from '../helper.js';
+import getRandNum from '../helper.js';
 import playGame from '../index.js';
 
 const gameDescription = 'What number is missing in the progression?';
 
-const generateProgression = () => {
-  let numberToPush = getRandomNumber(0, 20);
-  const randomStep = getRandomNumber(2, 5);
-  const randProgressionLength = getRandomNumber(5, 10);
-  const progression = [];
-  for (let i = 0; i < randProgressionLength; i += 1) {
-    numberToPush += randomStep;
-    progression.push(numberToPush);
+const getProgression = (startingElement, step, length) => {
+  const progression = [startingElement];
+  let element = startingElement;
+  for (let i = 1; i < length; i += 1) {
+    element += step;
+    progression.push(element);
   }
   return progression;
 };
 
 const getQuestionAndAnswer = () => {
-  const generatedProgression = generateProgression();
-  const hideIndex = getRandomNumber(0, generatedProgression.length - 1);
-  const correctAnswer = String(generatedProgression[hideIndex]);
+  const generatedProg = getProgression(getRandNum(0, 20), getRandNum(2, 5), getRandNum(5, 10));
+  const hideIndex = getRandNum(0, generatedProg.length - 1);
+  const correctAnswer = String(generatedProg[hideIndex]);
 
   let outputString = '';
-  for (let i = 0; i < generatedProgression.length; i += 1) {
+  for (let i = 0; i < generatedProg.length; i += 1) {
     if (i === hideIndex) outputString += ' ..';
-    else outputString += ` ${generatedProgression[i]}`;
+    else outputString += ` ${generatedProg[i]}`;
   }
   const question = outputString;
   return [question, correctAnswer];
